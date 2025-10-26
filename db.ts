25676175632ffa4c410c75361076bc5daec81864
ZHE-kz <kuizhelin@gmail.com>
2025-10-26 18:44:45 +0800

    Add files via upload

---
+++ db.ts
@@ -0,0 +1,1 @@
+﻿interface Player{userId:string;qIndex:number;score:number;} const players=new Map<string,Player>(); export function getPlayer(userId:string):Player{ if(!players.has(userId)){ players.set(userId,{userId,userId,qIndex:0,score:0}); } return players.get(userId)!; } export function savePlayer(player:Player){ players.set(player.userId,player); }
